bd340b12983d37703bfeeaa7ec19b0e4cd573351
zhao0515 <110957684+zhao0515@users.noreply.github.com>
2023-12-02 01:06:02 +0800

    commit 1

---
+++ lists_warmup.py
@@ -0,0 +1,20 @@
+#1.
+#my_numbers = ['3', '1', '4', '1', '5', '9', '2']
+#my_numbers[0] = 10
+#print(my_numbers)
+#2.
+#my_numbers = ['3', '1', '4', '1', '5', '9', '2']
+#my_numbers[6] = 1
+#print(my_numbers)
+#3.
+#my_numbers = ['3', '1', '4', '1', '5', '9', '2']
+#my_numbers.pop(0)
+#my_numbers.pop(0)
+#print(my_numbers)
+#4.
+#my_numbers = ['3', '1', '4', '1', '5', '9', '2']
+#i = 9
+#if i in my_numbers:
+#    print("exist")
+#else:
+#    print("not exist")
